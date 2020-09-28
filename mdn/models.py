af427d52fc302d07918a06c87f7524df5692c3eb
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2020-09-28 14:44:31 +0300

    Write independent blocks

--- mdn/models.py
+++ mdn/models.py
@@ -3,22 +3,24 @@ import torch.nn as nn
 from torch.distributions import Normal, OneHotCategorical
 
 
-class MixtureDensityNetwork(nn.Module):
+class MixtureDensity(nn.Module):
     """
     Mixture density network.
-
     [ Bishop, 1994 ]
 
-    Parameters
-    ----------
-    dim_in: int; dimensionality of the covariates
-    dim_out: int; dimensionality of the response variable
-    n_components: int; number of components in the mixture model
+    Take as input a tensor of shape [N, *, dim_in] and produce
+    a gaussian mixture distribution over tensors of shape
+    [N, *, dim_out].
+
+    Args:
+        dim_in (int): dimensionality of the covariates
+        dim_out (int): dimensionality of the response variable
+        n_components (int): number of components in the mixture model
     """
     def __init__(self, dim_in, dim_out, n_components):
         super().__init__()
-        self.pi_network = CategoricalNetwork(dim_in, n_components)
-        self.normal_network = MixtureDiagNormalNetwork(dim_in, dim_out,
+        self.pi_network = Categorical(dim_in, n_components)
+        self.normal_network = MixtureDiagNormal(dim_in, dim_out,
                                                        n_components)
 
     def forward(self, x):
@@ -26,49 +28,78 @@ class MixtureDensityNetwork(nn.Module):
 
     def loss(self, x, y):
         pi, normal = self.forward(x)
-        loglik = normal.log_prob(y.unsqueeze(1).expand_as(normal.loc))
-        loglik = torch.sum(loglik, dim=2)
-        loss = -torch.logsumexp(torch.log(pi.probs) + loglik, dim=1)
+        loglik = normal.log_prob(y.unsqueeze(-1).expand_as(normal.loc))
+        loglik = torch.sum(loglik, dim=-1)
+        loss = -torch.logsumexp(torch.log(pi.probs) + loglik, dim=-1)
         return loss
 
     def sample(self, x):
         pi, normal = self.forward(x)
-        samples = torch.sum(pi.sample().unsqueeze(2) * normal.sample(), dim=1)
+        samples = torch.sum(pi.sample().unsqueeze(-1) * normal.sample(), dim=1)
         return samples
 
 
-class MixtureDiagNormalNetwork(nn.Module):
+class MixtureDensityNetwork(MixtureDensity):
+    """
+    Mixture density network.
+    [ Bishop, 1994 ]
+    Parameters
+    ----------
+    dim_in: int; dimensionality of the covariates
+    dim_out: int; dimensionality of the response variable
+    n_components: int; number of components in the mixture model
+    """
+    def __init__(self, dim_in, dim_out, n_components):
+        super().__init__(dim_in, dim_out, n_components)
 
-    def __init__(self, in_dim, out_dim, n_components, hidden_dim=None):
+        self.hidden_layer = nn.Sequential(
+            nn.Linear(dim_in, dim_in),
+            nn.ELU()
+        )
+
+    def forward(self, x):
+        return super().forward(self.hidden_layer(x))
+
+    def loss(self, x, y):
+        pi, normal = self.forward(x)
+        loglik = normal.log_prob(y.unsqueeze(-1).expand_as(normal.loc))
+        loglik = torch.sum(loglik, dim=-1)
+        loss = -torch.logsumexp(torch.log(pi.probs) + loglik, dim=-1)
+        return loss
+
+    def sample(self, x):
+        pi, normal = self.forward(x)
+        samples = torch.sum(pi.sample().unsqueeze(-1) * normal.sample(), dim=1)
+        return samples
+
+
+class MixtureDiagNormal(nn.Module):
+
+    def __init__(self, in_dim, out_dim, n_components):
         super().__init__()
         self.n_components = n_components
-        if hidden_dim is None:
-            hidden_dim = in_dim
-        self.network = nn.Sequential(
-            nn.Linear(in_dim, hidden_dim),
-            nn.ELU(),
-            nn.Linear(hidden_dim, 2 * out_dim * n_components),
-        )
+        self.out_dim = out_dim
+        self.in_dim = in_dim
+
+        self.mean_net = nn.Linear(in_dim, out_dim * n_components)
+        self.std_net = nn.Linear(in_dim, out_dim * n_components)
 
     def forward(self, x):
-        params = self.network(x)
-        mean, sd = torch.split(params, params.shape[1] // 2, dim=1)
-        mean = torch.stack(mean.split(mean.shape[1] // self.n_components, 1))
-        sd = torch.stack(sd.split(sd.shape[1] // self.n_components, 1))
-        return Normal(mean.transpose(0, 1), torch.exp(sd).transpose(0, 1))
+        mean, std = self.mean_net(x), self.std_net(x)
+        mean = mean.view(*mean.shape[:-1], self.out_dim, self.n_components)
+        std = std.view(*std.shape[:-1], self.out_dim, self.n_components)
+        return Normal(mean, torch.exp(std))
 
-class CategoricalNetwork(nn.Module):
 
-    def __init__(self, in_dim, out_dim, hidden_dim=None):
+class Categorical(nn.Module):
+
+    def __init__(self, in_dim, out_dim):
         super().__init__()
-        if hidden_dim is None:
-            hidden_dim = in_dim
-        self.network = nn.Sequential(
-            nn.Linear(in_dim, hidden_dim),
-            nn.ELU(),
-            nn.Linear(hidden_dim, out_dim)
-        )
+        self.out_dim = out_dim
+        self.in_dim = in_dim
+
+        self.categories_net = nn.Linear(in_dim, out_dim)
 
     def forward(self, x):
-        params = self.network(x)
-        return OneHotCategorical(logits=params)
+        logits = self.categories_net(x)
+        return OneHotCategorical(logits=logits)
